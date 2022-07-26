ac7c91afd4ac8c6d5db1ccbb83490adf5d2250cd
Daniel Kang <danieljiwonkang98@gmail.com>
2022-07-26 13:15:41 +0900

    Display Image in Chat Room

--- lib/models/chat_room_route.dart
+++ lib/models/chat_room_route.dart
@@ -4,6 +4,7 @@ import 'package:app/components/message_field.dart';
 import 'package:app/components/padding.dart';
 import 'package:app/controllers/authentication_controller.dart';
 import 'package:app/requests/message_requests.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sendbird_sdk/sendbird_sdk.dart';
@@ -112,6 +113,50 @@ class ChatRoomRouteState extends State<ChatRoomRoute> {
                   shrinkWrap: true,
                   itemCount: messages.data?.length,
                   itemBuilder: (BuildContext context, int index) {
+                    late Widget titleWidget;
+                    if (messages.data?[index] is UserMessage) {
+                      titleWidget = Text(
+                        messages.data?[index].message ?? 'Empty Text',
+                        textAlign: messages.data?[index].sender?.userId ==
+                                _authentication.currentUser?.userId
+                            ? TextAlign.right
+                            : TextAlign.left,
+                      );
+                    } else if (messages.data?[index] is FileMessage) {
+                      titleWidget = Expanded(
+                        child: Row(
+                          mainAxisAlignment:
+                              messages.data?[index].sender?.userId ==
+                                      _authentication.currentUser?.userId
+                                  ? MainAxisAlignment.end
+                                  : MainAxisAlignment.start,
+                          children: [
+                            CachedNetworkImage(
+                              height: 120,
+                              width: 180,
+                              fit: BoxFit.cover,
+                              imageUrl: (messages.data?[index] as FileMessage)
+                                      .secureUrl ??
+                                  (messages.data?[index] as FileMessage).url,
+                              placeholder: (context, url) => const SizedBox(
+                                width: 30,
+                                height: 30,
+                                child: Center(
+                                  child: CircularProgressIndicator(
+                                    valueColor: AlwaysStoppedAnimation<Color>(
+                                        Colors.white),
+                                  ),
+                                ),
+                              ),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      printError(info: 'Unknown Message Type');
+                    }
                     return ListTile(
                       isThreeLine: true,
                       leading: messages.data?[index].sender?.userId ==
@@ -122,13 +167,7 @@ class ChatRoomRouteState extends State<ChatRoomRoute> {
                               _authentication.currentUser?.userId
                           ? const Icon(Icons.person)
                           : null,
-                      title: Text(
-                        messages.data?[index].message ?? 'Empty Text',
-                        textAlign: messages.data?[index].sender?.userId ==
-                                _authentication.currentUser?.userId
-                            ? TextAlign.right
-                            : TextAlign.left,
-                      ),
+                      title: titleWidget,
                       subtitle: _channel!.channelType == ChannelType.group
                           ? Text(
                               'Unread ${(_channel as GroupChannel).getUnreadMembers(messages.data![index]).length}',
